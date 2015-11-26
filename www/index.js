e1f057018c7e97b50ac0402032fb9bb3539e2342
chpmobile <papc2_12@uni.worc.ac.uk>
2015-11-26 17:59:02 +0000

    fs
    fs

--- www/index.js
+++ www/index.js
@@ -2,12 +2,58 @@
 $(document).on('pageinit', function() {
 	
 	//set up listener for button click
-	$(document).on('click', getPosition);
+	$(document).on('click', checklocation);
+	// $(document).getElementById("turnon").on('click', checklocation);
+	// $(document).getElementById("turnof").on('click', clearlocation);
 	
 	//change time box to show message
 	$('#time').val("Press the button to get location data");
 	
 });
+
+function checklocation(){
+
+	var watchID = navigator.geolocation.watchPosition(success, fail, locationOptions);
+}
+
+function clearlocation(){ 
+	navigator.geolocation.clearWatch(watchID);
+
+}
+
+function success(position) { 
+		 //do something with the position 
+		 //You can find out more details about what the position obejct contains here:
+	// http://www.w3schools.com/html/html5_geolocation.asp
+	
+
+	//lets get some stuff out of the position object
+	var time = position.timestamp;
+	var latitude = position.coords.latitude;
+	var unixtime = new Date(position.timestamp); 
+    var date = unixtime.toDateString();
+
+
+    
+	
+	//OK. Now we want to update the display with the correct values
+	$('#time').val("Recieved data at " + unixtime);
+	$('#lattext').val(latitude);
+	$('#date').val("date is..." + date);
+	
+	} 
+	function fail(error) { 
+		 //do something with the error 
+		 //change time box to show updated message
+	$('#time').val("Error getting data: " + error);
+	}
+
+function locationOptions(position) {
+
+var locationOptions = { maximumAge: 3000, timeout: 4000, enableHighAccuracy: true };
+		$('#loc').val(locationOptions);
+
+}
 
 
 //Call this function when you want to get the current position
@@ -18,6 +64,7 @@ function getPosition() {
 	
 	//instruct location service to get position with appropriate callbacks
 	navigator.geolocation.getCurrentPosition(successPosition, failPosition);
+
 }
 
 
@@ -33,6 +80,8 @@ function successPosition(position) {
 	var latitude = position.coords.latitude;
 	var unixtime = new Date(position.timestamp); 
     var date = unixtime.toDateString();
+    var locationOptions = { maximumAge: 10000, timeout: 6000, enableHighAccuracy: true };
+
 	
 	//OK. Now we want to update the display with the correct values
 	$('#time').val("Recieved data at " + unixtime);
